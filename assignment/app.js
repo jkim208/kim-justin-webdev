c24c5ef4def674bcc9bb28b9da079d72d7c7bcd2
Justin Kim <justinokim@gmail.com>
2017-07-24 14:40:22 -0400

    test/app.js update

--- assignment/app.js
+++ assignment/app.js
@@ -1,22 +1,19 @@
 var app = require('../express');
 
+var mongoose = require('mongoose');
+mongoose.Promise = require('q').Promise;
+var connectionString = 'mongodb://localhost/webdev_summer1_2017';
+
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds143131.mlab.com:43131/heroku_fq4lg298'; // user specific
+}
+
+mongoose.connect(connectionString);
+
 require('./services/user.service.server.js');
 require('./services/website.service.server.js');
 require('./services/page.service.server.js');
 require('./services/widget.service.server.js');
-
-app.get('/goodbye', sayHello);
-app.get('/websites', sendWebsites);
-
-function sendWebsites(req, res) {
-    var websites = [
-        {name: 'facebook'},
-        {name: 'twitter'},
-        {name: 'linkedin'}
-    ];
-    res.send(websites);
-}
-
-function sayHello() {
-    console.log('hello');
-}
